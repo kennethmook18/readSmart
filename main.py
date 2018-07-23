ba6ff1758cd6ada7f127e51ce9cac72058859469
Federick Gonzalez <federickg23@gmail.com>
2018-07-23 16:11:43 -0400

    i made some changes to create a class

--- main.py
+++ main.py
@@ -29,20 +29,21 @@ class CssiUser(ndb.Model):
 class Books(ndb.model):
 	title = ndb.StringProperty()
 	author = ndb.StringProperty()
-	publication date = ndb.StringProperty()
 	id = ndb.StringProperty()
-	synopsis = ndb.StringProperty()
+	persons_input = ndb.IntegerProperty()
+	bookindex = ndb.IntegerProperty(repeat=True)
+
 
 class HomePage(webapp2.RequestHandler):
 	def get(self):
 		content = TEMPLATE.get_template('/templates/home.html')
 		self.response.write(content.render(active = logged_in))
-		hamlet = Books(
-			title = "hamlet"
-			author = "shakespeare"
-		)
-		Macbeth = books()
-		self.response.write(content.render(title= hamlet.title, author = hamlet.author))
+		# hamlet = Books(
+		# 	title = "hamlet"
+		# 	author = "shakespeare"
+		# )
+		# Macbeth = books()
+		# self.response.write(content.render(title= hamlet.title, author = hamlet.author))
 
 
 class MainHandler(webapp2.RequestHandler):
@@ -111,6 +112,14 @@ class LoginHandler(webapp2.RequestHandler):
 		# ndb.Query()
 		# q.filter("username = ", self.response.get("Username"))
 		# print q
+
+def average(persons_input, title):
+	b = books.query().fetch()
+	for book in b:
+		if b.title == title:
+			book_length = persons_input
+			book_length = int(book_length)
+			b.bookindex.append(book_length)
 
 
 app = webapp2.WSGIApplication([
