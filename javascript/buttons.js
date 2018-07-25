76b5f55a0eca2e4f0733fff297f084fc32c128e7
Federick Gonzalez <federickg23@gmail.com>
2018-07-25 11:37:43 -0400

    Tried and failed to make a js file work

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -1,9 +1,20 @@
-const yes = document.querySelector('yes');
-const no = document.querySelector('no');
+const yes = document.querySelector('#yes');
+const no = document.querySelector('#no');
 
 yes.addEventListener('click', () =>{
   console.log("i've been clicked")
-  document.querySelector("yesnobuttons").style.visibility = "hidden";
-  document.querySelector("userinput").style.visibility = "visible";
+  document.querySelector("#yesnobuttons").style.visibility = "hidden";
+  document.querySelector("#userinput").style.visibility = "visible";
+  document.querySelector("#average").style.visibility = "visible";
 
+  document.querySelector("#averageset").style.visibility = "hidden";
+
+});
+no.addEventListener('click', () =>{
+  console.log("i've been clicked")
+  document.querySelector("#yesnobuttons").style.visibility = "hidden";
+  document.querySelector("#userinput").style.visibility = "hidden";
+  document.querySelector("#average").style.visibility = "visible";
+
+  document.querySelector("#averageset").style.visibility = "hidden";
 });
